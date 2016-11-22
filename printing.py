2aa4d31208acbcf1aa6cc4ba255aedc322d9cfb5
László Székely-Tóth <notisracer@gmail.com>
2016-11-22 15:09:09 +0100

    added count_games

--- printing.py
+++ printing.py
@@ -1,3 +1,9 @@
 
 # Printing functions
 """ printing.py: for printing the output of the report functions. You can use this to test your solutions. """
+
+import reports
+
+file_name = "game_stat.txt"
+
+print(reports.count_games(file_name))
--- reports.py
+++ reports.py
@@ -1,3 +1,10 @@
 
 # Report functions
 """ reports.py: write only the report functions in it. """
+
+
+def count_games(file_name):  # 1. How many games are in the file?
+    """ Returns how many games are in the file """
+    with open(file_name) as f:
+        data_list = [line.strip().split('\t') for line in f]
+    return len(data_list)
