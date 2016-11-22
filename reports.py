00aa85ea9ea768c2c345877116bc501cd2ed5bfe
László Székely-Tóth <notisracer@gmail.com>
2016-11-22 16:01:18 +0100

    added count_by_genre

--- reports.py
+++ reports.py
@@ -1,6 +1,7 @@
 
 # Report functions
 """ reports.py: write only the report functions in it. """
+# Minecraft->23->2009->Survival game->Microsoft
 
 
 def count_games(file_name):  # 1. How many games are in the file?
@@ -24,3 +25,9 @@ def get_latest(file_name):  # 3. Which was the latest game?
         data_list = [line.strip().split('\t') for line in f]
     data_list.sort(key=lambda data: data[2], reverse=True)
     return data_list[0][0]
+
+
+def count_by_genre(file_name, genre):  # 4. How many games do we have by genre?
+    with open(file_name) as f:
+        genre_counter = sum([1 for line in f if line.strip().lower().split('\t')[3] == str(genre).lower()])
+    return genre_counter
