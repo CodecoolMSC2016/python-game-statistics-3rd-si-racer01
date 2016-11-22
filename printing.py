5f29324f7f9a2af76a7cf1d46db540d8350fe7df
László Székely-Tóth <notisracer@gmail.com>
2016-11-22 15:49:18 +0100

    beautified pringing even more

--- printing.py
+++ printing.py
@@ -8,5 +8,6 @@ file_name = "game_stat.txt"
 year = 2009
 
 print("1.", reports.count_games(file_name))
-print("  ", reports.decide(file_name, int(input("2. Which year? "))))
+year = int(input("2. Which year? "))
+print("  ", reports.decide(file_name, year))
 print("3.", reports.get_latest(file_name))
