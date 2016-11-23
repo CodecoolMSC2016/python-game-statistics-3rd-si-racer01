24d77bdd45c06a7f160add9c3130a1bb1d6b4ca6
László Székely-Tóth <notisracer@gmail.com>
2016-11-23 16:47:33 +0100

    added get_most_played and sum_sold

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,38 @@
 
 # Printing functions
+""" printing.py: for printing the output of the report functions. You can use this to test your solutions. """
+
+import reports
+
+
+def check_file(file_name, fallback_file_name=""):
+    try:
+        f = open(file_name)
+    except FileNotFoundError as e:
+        print(e)
+        if fallback_file_name:
+            check = check_file(fallback_file_name)
+            print("[NOTE] Using default file")
+            return check
+        else:
+            raise
+    else:
+        f.close()
+        return file_name
+
+
+def printing():
+    fallback_file_name = "game_stat.txt"
+    file_name = input("Which file should I analyze? (default: game_stat.txt) ")
+    if not file_name:
+        file_name = fallback_file_name
+    try:
+        file_name = check_file(file_name, fallback_file_name)
+    except FileNotFoundError as e:
+        print("No such file. Exiting...")
+    else:
+        print("1.", reports.get_most_played(file_name))
+        print("2.", reports.sum_sold(file_name))
+
+if __name__ == "__main__":
+    printing()
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,36 @@
 
 # Report functions
+""" reports.py: write only the report functions in it. """
+# Minecraft->23->2009->Survival game->Microsoft
+
+
+def get_most_played(file_name):  # 1. What is the title of the most played game?
+    with open(file_name) as f:
+        most_played_list = [(line.split('\t')[0].strip(), float(line.split('\t')[1].strip())) for line in f]
+    most_played_list.sort(key=lambda game: game[1], reverse=True)
+    return most_played_list[0][0]
+
+
+def sum_sold(file_name):  # 2. How many copies have been sold total?
+    with open(file_name) as f:
+        total_sold = sum([float(line.split('\t')[1].strip()) for line in f])
+    return total_sold
+
+
+def get_selling_avg(file_name):  # 3. What is the average selling?
+    return int()
+    # Other expectation: if there is more than one, then return the first from the file
+
+
+def count_longest_title(file_name):  # 4. How many characters long is the longest title?
+    return int()
+
+
+def get_date_avg(file_name):  # 5. What is the average of the release dates?
+    return int(avg_year)
+    # Other expectation: the return value must be the rounded up average
+
+
+def get_game(file_name, title):  # 6. What properties has a game?
+    pass
+    # Expected output of the function: a list of all the properties of the game (a list of various type).
